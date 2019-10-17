4cec5a9a638b509a29cac8097654966ea691ff4e
tagilov <54817460+tagilov@users.noreply.github.com>
2019-10-17 19:50:52 +0500

    глянь пожалуйста

---
+++ js/skripts-nerds.js
@@ -0,0 +1,54 @@
+window.onload = function() {
+      var link = document.querySelector(".button-link");
+      var popup = document.querySelector(".form");
+      var close = document.querySelector(".modal-close");
+      var fullName = popup.querySelector("[name='fullName']");
+      var mail = popup.querySelector("[name=mail]");
+      var text = popup.querySelector("[name=text]");
+      var form = popup.querySelector("form");
+      var storage = localStorage.getItem("fullName");
+
+      var isStorageSupport = true;
+      var storage = "";
+
+      try {
+        storage = localStorage.getItem("fullName");
+      } catch (err) {
+        isStorageSupport = false;
+      }
+
+      link.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        popup.classList.add("form-show");
+        fullName.focus();
+        if (storage) {
+          fullName.value=storage;
+          mail.focus();
+        }
+      });
+
+      close.addEventListener("click", function (evt) {
+        evt.preventDefault();
+        popup.classList.remove("form-show");
+      });
+
+      form.addEventListener("submit", function (evt) {
+        if (!fullName.value || !mail.value || !text.value) {
+        evt.preventDefault();
+        console.log("нужно ввести имя и емаил");
+        } else {
+          if (isStorageSupport) {
+          localStorage.setItem("fullName",fullName.value);
+        }
+      }
+      });
+
+      window.addEventListener("keydown", function(evt) {
+        if (evt.keyCode ===27) {
+          if (popup.classList.contains("form-show")) {
+            evt.preventDefault();
+            popup.classList.remove("form-show");
+          }
+        }
+      })
+    }
