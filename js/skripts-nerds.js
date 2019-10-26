da29c64fd710a69820af3200cd8959f837ed342a
tagilov <54817460+tagilov@users.noreply.github.com>
2019-10-26 19:55:31 +0500

    5

--- js/skripts-nerds.js
+++ js/skripts-nerds.js
@@ -1,56 +1,53 @@
-window.onload = function() {
-  var link = document.querySelector(".button-link");
-  var popup = document.querySelector(".form");
-  var close = document.querySelector(".modal-close");
-  var fullName = popup.querySelector("[name='fullName']");
-  var mail = popup.querySelector("[name=mail]");
-  var text = popup.querySelector("[name=text]");
-  var form = popup.querySelector("form");
-  var storage = localStorage.getItem("fullName");
+var link = document.querySelector(".button-link");
+var popup = document.querySelector(".form");
+var close = document.querySelector(".modal-close");
+var fullName = popup.querySelector("[name='fullName']");
+var mail = popup.querySelector("[name=mail]");
+var text = popup.querySelector("[name=text]");
+var form = popup.querySelector("form");
+var storage = localStorage.getItem("fullName");
+var isStorageSupport = true;
+var storage = "";
 
-  var isStorageSupport = true;
-  var storage = "";
+try {
+  storage = localStorage.getItem("fullName");
+} catch (err) {
+  isStorageSupport = false;
+}
 
-  try {
-    storage = localStorage.getItem("fullName");
-  } catch (err) {
-    isStorageSupport = false;
+link.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.add("form-show");
+  fullName.focus();
+  if (storage) {
+    fullName.value = storage;
+    mail.focus();
   }
+});
 
-  link.addEventListener("click", function(evt) {
+close.addEventListener("click", function(evt) {
+  evt.preventDefault();
+  popup.classList.remove("form-show");
+  popup.classList.remove("form-error");
+});
+
+form.addEventListener("submit", function(evt) {
+  if (!fullName.value || !mail.value || !text.value) {
     evt.preventDefault();
-    popup.classList.add("form-show");
-    fullName.focus();
-    if (storage) {
-      fullName.value = storage;
-      mail.focus();
+    popup.classList.add("form-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("fullName", fullName.value);
     }
-  });
+  }
+});
 
-  close.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    popup.classList.remove("form-show");
-    popup.classList.remove("form-error");
-  });
-
-  form.addEventListener("submit", function(evt) {
-    if (!fullName.value || !mail.value || !text.value) {
+window.addEventListener("keydown", function(evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("form-show")) {
       evt.preventDefault();
-      popup.classList.add("form-error");
-    } else {
-      if (isStorageSupport) {
-        localStorage.setItem("fullName", fullName.value);
-      }
+      popup.classList.remove("form-show");
+      popup.classList.remove("form-error");
     }
-  });
-
-  window.addEventListener("keydown", function(evt) {
-    if (evt.keyCode === 27) {
-      if (popup.classList.contains("form-show")) {
-        evt.preventDefault();
-        popup.classList.remove("form-show");
-        popup.classList.remove("form-error");
-      }
-    }
-  });
-};
+  }
+});
