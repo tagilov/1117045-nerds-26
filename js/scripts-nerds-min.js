5c0b69e9d83875de4d520a981b63c0046b72d3dd
tagilov <54817460+tagilov@users.noreply.github.com>
2019-10-31 16:42:27 +0500

    финишная прямая)))

--- js/scripts-nerds-min.js
+++ js/scripts-nerds-min.js
@@ -1,1 +1,38 @@
-var link=document.querySelector(".button-link"),popup=document.querySelector(".form"),close=document.querySelector(".modal-close"),fullName=popup.querySelector("[name='fullName']"),mail=popup.querySelector("[name=mail]"),text=popup.querySelector("[name=text]"),form=popup.querySelector("form"),storage=localStorage.getItem("fullName"),isStorageSupport=!0;storage="";try{storage=localStorage.getItem("fullName")}catch(e){isStorageSupport=!1}link.addEventListener("click",function(e){e.preventDefault(),popup.classList.add("form-show"),fullName.focus(),storage&&(fullName.value=storage,mail.focus())}),close.addEventListener("click",function(e){e.preventDefault(),popup.classList.remove("form-show"),popup.classList.remove("form-error")}),form.addEventListener("submit",function(e){fullName.value&&mail.value&&text.value?isStorageSupport&&localStorage.setItem("fullName",fullName.value):(e.preventDefault(),popup.classList.add("form-error"))}),window.addEventListener("keydown",function(e){27===e.keyCode&&popup.classList.contains("form-show")&&(e.preventDefault(),popup.classList.remove("form-show"),popup.classList.remove("form-error"))});
+var link = document.querySelector(".button-link"),
+  popup = document.querySelector(".form"),
+  close = document.querySelector(".modal-close"),
+  fullName = popup.querySelector("[name='fullName']"),
+  mail = popup.querySelector("[name=mail]"),
+  text = popup.querySelector("[name=text]"),
+  form = popup.querySelector("form"),
+  storage = localStorage.getItem("fullName"),
+  isStorageSupport = !0;
+storage = "";
+try {
+  storage = localStorage.getItem("fullName");
+} catch (e) {
+  isStorageSupport = !1;
+}
+link.addEventListener("click", function(e) {
+  e.preventDefault(),
+    popup.classList.add("form-show"),
+    fullName.focus(),
+    storage && ((fullName.value = storage), mail.focus());
+}),
+  close.addEventListener("click", function(e) {
+    e.preventDefault(),
+      popup.classList.remove("form-show"),
+      popup.classList.remove("form-error");
+  }),
+  form.addEventListener("submit", function(e) {
+    fullName.value && mail.value && text.value
+      ? isStorageSupport && localStorage.setItem("fullName", fullName.value)
+      : (e.preventDefault(), popup.classList.add("form-error"));
+  }),
+  window.addEventListener("keydown", function(e) {
+    27 === e.keyCode &&
+      popup.classList.contains("form-show") &&
+      (e.preventDefault(),
+      popup.classList.remove("form-show"),
+      popup.classList.remove("form-error"));
+  });
